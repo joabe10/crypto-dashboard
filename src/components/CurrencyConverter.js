4ce31a5641d2686fcfcccd574ad4ad92f92b0aea
Joabe <joabe.de.silva@gmail.com>
2023-06-09 16:23:51 -0300

    deletei alguns arquivos e criei um para o conversor de moeda e outro para newsfeed

---
+++ src/components/CurrencyConverter.js
@@ -0,0 +1,11 @@
+
+function CurrencyConverter() {
+    return (
+      <div className="currency-converter">
+        Currency Converter
+      </div>
+    );
+  }
+  
+  export default CurrencyConverter;
+  
---
+++ src/components/Newsfeed.js
@@ -0,0 +1,11 @@
+
+function Newsfeed() {
+    return (
+      <div className="news-feed">
+        Newsfeed
+      </div>
+    );
+  }
+  
+  export default Newsfeed;
+  
