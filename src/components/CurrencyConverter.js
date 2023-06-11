78fe48379fa578f805e4511cf270d183f9142079
Joabe <joabe.de.silva@gmail.com>
2023-06-11 16:30:43 -0300

    add array com criptos que gostaria de manipular e usei map() para manipular array

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,27 +1,44 @@
 import ExchangeRate from "./ExchangeRate";
 
 function CurrencyConverter() {
+    const currencies = ['BTC', 'ETH', 'USD', 'XRP', 'LTC', 'ADA']
+
     return (
 
       <div className="currency-converter">
         <h2>Currency Converter</h2>
         <div>
             <table>
-                <body>
-                    <tr>Primary Currency:</tr>
-                    <td>
-                        <input 
-                            type="number"
-                            name="currency-amount-1"
-                            value={""} 
-                        />                
-                    </td>
-                    <td>
-                        <select value={""} name="currency-option-1" className="currency-options">
-                            <option name="currency-option-1">Bitcoin</option>
-                        </select>
-                    </td>
-                </body>
+                <tbody>
+                    <tr>Primary Currency:
+                        <td>
+                            <input 
+                                type="number"
+                                name="currency-amount-1"
+                                value={""} 
+                            />                
+                        </td>
+                        <td>
+                            <select value={""} name="currency-option-1" className="currency-options">
+                              {currencies.map((currency, _index) => (<option key={_index}>{currency}</option>))}
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>Secondary Currency:
+                        <td>
+                            <input 
+                                type="number"
+                                name="currency-amount-2"
+                                value={""} 
+                            />                
+                        </td>
+                        <td>
+                            <select value={""} name="currency-option-2" className="currency-options">
+                                {currencies.map((currency) => (<option>{currency}</option>))}
+                            </select>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
         
